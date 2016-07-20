428ffe4a624b215ec00c449d03ac66286383f6d4
Sebastian Borrazas <seba.borrazas@gmail.com>
2016-07-20 15:29:28 -0300

    Update GAnalytics

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 provision-production:
 	ansible-playbook ansible/provision.yml \
     -i ansible/production_inventory \
+		--ask-vault-pass
 
 deploy-production:
 	ansible-playbook ansible/deploy.yml \
     -i ansible/production_inventory \
+		--ask-vault-pass
--- dev.webpack.config.js
+++ dev.webpack.config.js
@@ -34,7 +34,10 @@ module.exports = {
       },
       {
         test: /\.less$/,
-        loader: "style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!autoprefixer?browsers=last 2 version!less?outputStyle=expanded&sourceMap",
+        loader: ExtractTextPlugin.extract(
+          "style",
+          "css!less"
+        ),
       },
       {
         test: webpackIsomorphicToolsPlugin.regular_expression("images"),
